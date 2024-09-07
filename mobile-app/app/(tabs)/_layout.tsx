6071803b12dea5c0cee3c4d23916c7341ac08f64
ganeaflorin <ganeafloman@gmail.com>
2024-09-07 13:25:12 +0300

    mobile app initial setup

---
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name='view'
+        options={{
+          title: 'Vizualizează',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? 'book' : 'book-outline'}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='add'
+        options={{
+          title: 'Adaugă',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? 'add-circle' : 'add-circle-outline'}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ mobile-app/app/(tabs)/view.tsx
@@ -0,0 +1,16 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    // <ThemedView>
+    <ThemedText>Howdy</ThemedText>
+    // </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({});
