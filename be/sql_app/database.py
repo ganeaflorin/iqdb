564dffc67a816410918aa8868aa645718691a198
ganeaflorin <ganeafloman@gmail.com>
2024-10-22 21:32:38 +0300

    change postgres host, move database url to env

--- be/sql_app/database.py
+++ be/sql_app/database.py
@@ -1,12 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values 
+# loading variables from .env file
+load_dotenv() 
 
+# accessing and printing value
+DATABASE_URL = os.getenv("DATABASE_URL") 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5433/iqdb"
-
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5433/iqdb"
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
